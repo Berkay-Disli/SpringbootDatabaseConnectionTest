8d52c8fc6b676cc5f39ab2b49063fde84ceffd4a
berkaydisli <berkay.dsli@gmail.com>
2024-09-08 00:05:29 +0300

    Initialize

---
+++ src/test/java/com/berkaydisli/databaseconnectiontest/DatabaseconnectiontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkaydisli.databaseconnectiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseconnectiontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
